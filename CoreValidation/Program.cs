9758dc16c9f83879a402e1dc4ac22f8a0bbf3b2e
Malik Haseeb <malikhaseeb1935@gmail.com>
2021-08-20 00:02:27 +0500

    done some changes

--- CoreValidation/Program.cs
+++ CoreValidation/Program.cs
@@ -6,7 +6,7 @@ namespace CoreValidation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Haseeb!");
         }
     }
 }
